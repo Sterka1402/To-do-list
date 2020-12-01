70132b03eb763458c85ca20d280ffc7708e33974
Sterka1402 <kozlova.valya@gmail.com>
2020-12-01 14:23:11 +0300

    create button, styles, event onclick

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import './sass/main.scss';
+import './page/list';
 
 console.log('App is running');
---
+++ src/page/list.js
@@ -0,0 +1,16 @@
+const userList = document.querySelector("user-list");
+const addTask = document.getElementById("new-task");
+const addBtn = document.getElementById("add-task");
+
+
+const renderItem = () => {
+  console.log(addTask.value);
+
+  const newLi = document.createElement('li');
+  newLi.textContent = addTask.value;
+  userList.append(newLi);
+  addTask.value = '';
+};
+
+addBtn.addEventListener('click', renderItem);
+
