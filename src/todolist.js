1704f3a7bf492c8a73ee99591c50fcef7d83d038
Sterka1402 <kozlova.valya@gmail.com>
2020-12-04 16:32:58 +0300

    create removelist

--- src/todolist.js
+++ src/todolist.js
@@ -1,53 +1,63 @@
 const userList = document.querySelector('.user-list');
 const addTask = document.getElementById("new-task");
 const addBtn = document.getElementById("add-btn");
-const list = [];
+const list = [{text:'Blalala'},{text:'bla2'}];
 
 function addToList(e) {
   if (addTask.value !== '') {
-  let id = 0;
-  const text = addTask.value;
-  const item = {
-    id,
-    text,
-    done: false,
-  };
-  id++;
-  list.push(item);
+     const text = addTask.value;
+     const item = {
+      text,
+      done: false,
+    };
+    list.push(item);
   renderList(list,userList);
   addTask.value = '';
   }
   return
 }
 
-console.log(list);
+// console.log(list);
+
+function removeFromList(e) {
+  console.log(e.target.tagName);
+   list.forEach(() => {
+      if (e.target.tagName === 'svg') {
+        // const parentLi = e.target.closest('LI');
+        // console.log(parentLi);
+        // if (item.text == parentLi.textContent) {
+          // const idRemove = item.id;
+          // console.log('id' + idRemove);
+          // list.splice(idRemove,1);
+          const parent = e.target.closest('li');
+          parent.remove();
+      }else {
+        return undefined;
+        // }  
+      }
+    }); 
+  
+  renderList(list,userList);
+  addTask.value = '';
+  return
+}
 
 function renderList (items = [], itemsList) {
-  itemsList.innerHTML = items.map((item, index) => {
-    console.log(index);
+  items.forEach(() =>{
+    itemsList.innerHTML = items.map((item, index) => {
+    // console.log(index);
     return `
       <li id="${index}"/>
-        ${item.text}
-      </li>
-    `;
+      <svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="trash-alt" class="svg-inline--fa fa-trash-alt fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M268 416h24a12 12 0 0 0 12-12V188a12 12 0 0 0-12-12h-24a12 12 0 0 0-12 12v216a12 12 0 0 0 12 12zM432 80h-82.41l-34-56.7A48 48 0 0 0 274.41 0H173.59a48 48 0 0 0-41.16 23.3L98.41 80H16A16 16 0 0 0 0 96v16a16 16 0 0 0 16 16h16v336a48 48 0 0 0 48 48h288a48 48 0 0 0 48-48V128h16a16 16 0 0 0 16-16V96a16 16 0 0 0-16-16zM171.84 50.91A6 6 0 0 1 177 48h94a6 6 0 0 1 5.15 2.91L293.61 80H154.39zM368 464H80V128h288zm-212-48h24a12 12 0 0 0 12-12V188a12 12 0 0 0-12-12h-24a12 12 0 0 0-12 12v216a12 12 0 0 0 12 12z"></path></svg>
+      ${item.text}</li>
+      `;
   }).join('');
-}
+});
+};
+
 addBtn.addEventListener('click',addToList);
 
-console.log(list);
+// console.log(list);
 
-// addBtn.addEventListener('click', renderItem);
-
-// const removeItem = (e) => {
-//   console.log(e.target.tagName);
-//   if  (e.target.done === 'SPAN') {
-//     console.log(e.target.tagName);
-//     const parentLi = e.target.closest('LI');
-//    parentLi.remove();
-//   } else {
-//     return undefined;
-//   }
-// }
-
-// userList.addEventListener('click',removeItem);
+userList.addEventListener('click',removeFromList);
 
