a0e512a3334bfc1cc2055722ebe0fcc155431100
Sterka1402 <kozlova.valya@gmail.com>
2020-12-14 10:39:20 +0300

    create input current day and number tasks

--- src/modules/todolist.js
+++ src/modules/todolist.js
@@ -3,15 +3,16 @@ import addToListEnter from './add-tolist-enter';
 import addToList from './add-tolist';
 import removeFromList from './remove-fromlist';
 import toggleDone from './toggle-done';
+import renderDay from './render-day';
 
 window.addEventListener('load', () => {
   const userList = document.querySelector('.user-list');
   const addBtn = document.getElementById('add-btn');
   const list = JSON.parse(localStorage.getItem('list')) || [];
   const addTask = document.getElementById('new-task');
-
+  
+  renderDay(list);
   renderList(list, userList);
-
   addTask.addEventListener('keydown', (e) => addToListEnter(e, list, userList, addTask));
   addBtn.addEventListener('click', (e) => addToList(e, list, userList, addTask));
   userList.addEventListener('click', (e) => removeFromList(e, list, userList));
