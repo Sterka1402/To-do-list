3750e00a122634b6783a1377b2ac1dc7e0cb9a47
Sterka1402 <kozlova.valya@gmail.com>
2020-12-14 10:41:58 +0300

    check errors

--- src/modules/render-day.js
+++ src/modules/render-day.js
@@ -1,14 +1,13 @@
-function renderDay (list) {
+function renderDay(list) {
   const currentDay = document.querySelector('.current-date');
   const curWeekDate = new Date().getDay();
-  const week = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+  const week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
   const day = new Date();
   currentDay.innerHTML = `
   <h1>Hello!</h1>
   <p>Today is ${week[curWeekDate]}, ${day.toLocaleDateString()}.</p>
   <p> You have ${list.length} tasks today!</p>
   `;
-  
-  }
+}
 
-  export default renderDay;
+export default renderDay;
--- src/modules/todolist.js
+++ src/modules/todolist.js
@@ -10,7 +10,7 @@ window.addEventListener('load', () => {
   const addBtn = document.getElementById('add-btn');
   const list = JSON.parse(localStorage.getItem('list')) || [];
   const addTask = document.getElementById('new-task');
-  
+
   renderDay(list);
   renderList(list, userList);
   addTask.addEventListener('keydown', (e) => addToListEnter(e, list, userList, addTask));
